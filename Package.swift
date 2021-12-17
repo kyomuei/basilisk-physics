16b04f04cc196e185d432cc7ba05691553103a74
kyomuei <kyomuei.4log3@gmail.com>
2021-12-17 17:46:01 +0900

    Add header comment

--- Package.swift
+++ Package.swift
@@ -1,4 +1,10 @@
 // swift-tools-version:5.1
+// -------------------------------------------------------------------------------------------------
+// This source file is part of the Basilisk Physics open source project.
+// Copyright 2021 kyomuei
+// SPDX-License-Identifier: Apache-2.0
+// See the LICENSE file in the project root for lisence information.
+// -------------------------------------------------------------------------------------------------
 
 import PackageDescription
 
